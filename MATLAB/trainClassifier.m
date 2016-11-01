9b1d81fe8c00ba4db555d19aa6562d983c424ce1
Irene <irene.l.feng.17@dartmouth.edu>
2016-10-31 20:44:18 -0400

    removed disgust

--- MATLAB/trainClassifier.m
+++ MATLAB/trainClassifier.m
@@ -4,10 +4,12 @@
 % X and Y
 load('X.mat');
 load('Y.mat'); 
-trainX = X(1:28709,:);
-trainY = Y(1:28709,:);
-testX = X(28710:32299,:);
-testY = Y(28710:32299,:);
+disgust = find(Y==1);
+% remove all disgust indexes Y==1
+trainX = X(setdiff(1:28709, disgust),:);
+trainY = Y(setdiff(1:28709, disgust),:);
+testX = X(setdiff(28710:32299, disgust),:);
+testY = Y(setdiff(28710:32299, disgust),:);
 [MDL, s, o] = model_selection(trainX, trainY, 1:3, [4, 8], 5);
 
 % get testX and testY 
