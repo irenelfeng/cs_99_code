055b355c2b4eec83eff707cf4611ae8a3534df35
irenelfeng <irene.l.feng.17@dartmouth.edu>
2016-11-16 10:01:20 -0500

    fixed model_top_bottom

---
+++ MATLAB/model_top_bottom_testing.m
@@ -0,0 +1,24 @@
+% test existing model with top blur and bottom blur 
+
+% loads existing model MDL here
+s=2;
+o=8;
+load(sprintf('MDLs%do%d.mat', s, o));
+
+load('topX.mat');
+load('bottomX.mat');
+load('Y.mat');
+
+d = date;
+testY =Y; % all of Y serves as testing. gonna be a whole ton.  
+
+for i=0:1 % top = 0, bottom = 1 
+    if (i == 0)
+        pred_Y = MDL_predict(MDL, topX, s, o);
+        confusion_matrix(pred_Y, testY, sprintf('confusiontop%s-s%ds-o%d.png', d, s, o));
+    else
+        pred_Y = MDL_predict(MDL, bottomX, s, o);
+        confusion_matrix(pred_Y, testY, sprintf('confusionbottom%s-s%ds-o%d.png', d, s, o));
+    end
+    
+end
