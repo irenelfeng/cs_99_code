0fda615b6efd1ca662dd81c97b238da2171c2213
Irene <irene.l.feng.17@dartmouth.edu>
2016-11-15 21:11:30 -0500

    saving files issues

--- MATLAB/trainClassifier.m
+++ MATLAB/trainClassifier.m
@@ -1,7 +1,7 @@
 % given X, Y, returns model and confusion matrix 
 % calls model_selection.m, confusion_matrix.m
-% this is for LDA, change MDL_predict to SVM  
-% comment out 
+% change this line from LDA to SVM
+type = 'SVM';
 
 % gets date 
 d = date; 
@@ -15,9 +15,9 @@ trainX = X(setdiff(1:28709, disgust),:);
 trainY = Y(setdiff(1:28709, disgust));
 testX = X(setdiff(28710:32299, disgust),:);
 testY = Y(setdiff(28710:32299, disgust));
-[MDL, s, o] = model_selection(trainX, trainY, 1:3, [4, 8], 5);
+[MDL, s, o] = model_selection(trainX, trainY, 1:3, [4, 8], 5, type);
 
 % get testX and testY 
 predY = MDL_predict(MDL, testX, s, o);
 save(sprintf('MDL_SVMs%do%d', s, o), 'MDL');
-confusion_matrix(predY, testY, [d,'-s',s,'-o',o,'.png']);
+confusion_matrix(predY, testY, sprintf('confusion%s-s%ds-o%d.png', d, s, o));
