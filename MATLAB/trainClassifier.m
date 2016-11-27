2f45278c50d5c14f0e142be5c3c8b84ee73046b5
irenelfeng <irene.l.feng.17@dartmouth.edu>
2016-11-27 18:20:51 -0500

    code in place to train on either bottom or top. have to do some adding of topX/bottomX/X though

--- MATLAB/trainClassifier.m
+++ MATLAB/trainClassifier.m
@@ -7,17 +7,21 @@ type = 'LDA';
 d = date; 
 
 % X and Y
-load('X.mat'); % images
+load('topX.mat'); % images
+%load('bottomX.mat');
 load('Y.mat');  % labels
 disgust = find(Y==1);
 neutral = find(Y==6);
 % remove all disgust indexes Y==1
-trainX = X(setdiff(1:28709, [disgust neutral]),:);
-trainY = Y(setdiff(1:28709, [disgust neutral]));
-testX = X(setdiff(28710:32299, [disgust neutral]),:);
-testY = Y(setdiff(28710:32299, [disgust neutral]));
-[MDL, s, o, comps] = model_selection(trainX, trainY, 1:3, [4, 8], 5, type);
+trainX = topX(setdiff(1:28709, [disgust; neutral]),:);
+trainY = Y(setdiff(1:28709, [disgust; neutral]));
+testX = topX(setdiff(1:32299, [disgust; neutral]),:);
+testY = Y(setdiff(1:32299, [disgust; neutral]));
+[MDL, s, o] = model_selection(trainX, trainY, 1:3, [4, 8], 5, type);
 
-predY = MDL_predict(MDL, testX, s, o, comps);
-save(sprintf('MDL_%sPCAs%do%d', type, s, o), 'MDL');
-confusion_matrix(predY, testY, sprintf('confusion%s-s%ds-o%d.png', d, s, o));
+predY = MDL_predict(MDL, testX, s, o);
+%predY = MDL_predict(MDL, testX, s, o, comps);
+
+save(sprintf('MDL_BOTTOM%ss%do%d', type, s, o), 'MDL');
+confusion_matrix(predY, testY, sprintf('confusionBottomTrained%s-s%ds-o%d%s.png', type, s, o,d), ...
+                sprintf('Confusion BottomTrained %s-s%d-o%d', type, s, o));
--- MATLAB/trainSVMshort.m
+++ MATLAB/trainSVMshort.m
@@ -16,20 +16,22 @@ features = zeros(size(trainX, 1), si*or*100*2); % grid size * 2.
 for i=1:size(X,1)
     features(i, :) = image_features(X(i,:), si, or)'; % call image features on each file 
 end
-% get the mean for the features 
-[comps, pca_features, resid] = bestPCA(features); 
-
-disp('finished creating features!');
 
 %% train with these features 
-MDL = train_Mc_SVM(pca_features(setdiff(1:28709, disgust),:), trainY);
-save(sprintf('MDL_SVMPCAs%do%d', si, or), 'MDL');
-save(sprintf('MDL_comps%do%d', si, or), 'comps');
+MDL = train_Mc_SVM(features(setdiff(1:28709, disgust),:), trainY);
+predY =  MDL_predict(MDL, testX, si, or);
+% uncomment next 4 lines (and comment previous 2 lines)  if you want pca
+% [comps, pca_features, resid] = bestPCA(features); 
+% sprintf('reduced to %d features', size(comps,2))
+% MDL = train_Mc_SVM(pca_features(setdiff(1:28709, disgust),:), trainY);
+% save(sprintf('MDL_comp s%do%d', si, or), 'comps');
+% predY = MDL_predict(MDL, testX, si, or, comps);
 
-predY = MDL_predict(MDL, testX, si, or, comps);
+save(sprintf(
+'MDL_SVMs%do%d', si, or), 'MDL'); % add PCA to line if you want PCA
 
 error = sum(testY - predY ~= 0)/(length(predY));
-confusion_matrix(predY, testY, sprintf('confusionSVMandPCA%s-s%ds-o%d.png', d, si, or), sprintf('SVM Whole, s%ds-o%d', si, or));
+confusion_matrix(predY, testY, sprintf('confusionSVMandPCA%s-s%ds-o%d.png', size(comps,2), si, or), sprintf('SVM Whole, s%ds-o%d', si, or));
 
 
     
