6d9ecac44a673373346791b286a579da98f08acd
Irene <irene.l.feng.17@dartmouth.edu>
2017-01-03 13:33:12 -0600

    first iteration mollahosseini

--- PYTHON/caffe_scripts/test_mollahosseini.py
+++ PYTHON/caffe_scripts/test_mollahosseini.py
@@ -2,12 +2,16 @@
 import caffe
 import numpy as np
 from PIL import Image
+import scipy.io as sio
 
 PARENT_DIR = '/Users/irenefeng/Documents/Computer_Social_Vision/'
 CAFFE_DIR = PARENT_DIR + 'caffe/'
 IMAGE_DIR = PARENT_DIR + 'cs_99_code/MATLAB/fer2013imgs/'
+FER_CSV = PARENT_DIR + 'cs_99_code/MATLAB/fer2013/fer2013.csv'
+
 net = caffe.Net(CAFFE_DIR + 'models/mollahosseini_fer/deploy.prototxt', 1,
-                weights=CAFFE_DIR + 'models/mollahosseini_fer/training_snapshot__iter_100000.caffemodel')
+								weights=CAFFE_DIR + 'models/mollahosseini_fer/training_snapshot__iter_100000.caffemodel')
+
 
 # net = caffe.Net(CAFFE_DIR+'models/bvlc_reference_caffenet/deploy.prototxt', 1,
 #  weights=CAFFE_DIR+'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel')
@@ -20,16 +24,54 @@ transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
 transformer.set_transpose('data', (2,0,1)) # i don't know what the transpose is for 
 transformer.set_channel_swap('data', (2,1,0)) # from RGB to BGR order 
 transformer.set_raw_scale('data', 255.0) # just in case not 0-255. 
+labels_fer = {
+	0:'Anger', # hand checked this!
+	1:'Disgust', 
+	2:'Fear', # hand checked this!
+	3:'Happiness', # hand checked this!
+	4:'Sad', # this is 5?
+	5:'Surprise', # this is 6? 
+	6:'Neutral' # need to check this one w/: pretty sure this is 4 in Mollahosseini
+ }
+
+test_set = (28710, 32300) 
+predictions = []
+# modes = ['original', 'bottomblur', 'topblur']
+# names = ['whole', 'top', 'bottom']
+modes = ['original']
+names = ['whole']
+for n in range(len(modes)):
+	for i in range(test_set[0], test_set[1]):
+		#load the image in the data layer
+		im = caffe.io.load_image(IMAGE_DIR + '{0}/{1}.png'.format(modes[n],i))
+		# generate crops 
+		crops = caffe.io.oversample([im], (40, 40))
+		# uncomment if not using crops 
+		#net.blobs['data'].reshape(1,3,120,120) 
+		#net.blobs['data'].data[...] = transformer.preprocess('data', im)
+		for j in range(10):
+			net.blobs['data'].data[j, ...] = transformer.preprocess('data', crops[j])
+		
+		out = net.forward() 
+		p = np.argmax(np.average(out['loss1/classifier'], axis=0))
+		predictions.append(p)
+		# print 'label for {0}, {1}'.format(i, p)
+
+		if (i % 100 == 0):
+			print 'finished testing for image {0}'.format(i) 
+
+	predictions = np.array(predictions)
+	labels = np.loadtxt(open(FER_CSV,"rb"),delimiter=",",usecols=[0],skiprows=1)
+	# starts counting from 0... so -1
+	acc_set = labels[test_set[0] - 1:test_set[1] - 1]
+	acc = (len(acc_set) - np.count_nonzero(acc_set - predictions)) * 1.0 / len(acc_set)
+	print 'accuracy is {0}'.format(acc)
+
+	sio.savemat('mollahosseini_test_results_{2}_{0}_{1}'.format(test_set[0], test_set[1], names[n]),
+	 			{'predY':predictions, 'testY':acc_set})
+
+# in matlab, then we can call confusion_matrix(predY, testY, stringpng, stringtitle)
+# for confusion matrix
 
 
-#load the image in the data layer
-im = caffe.io.load_image(IMAGE_DIR + 'original/1.png')
-# generate crops 
-crops = caffe.io.oversample([im], (40, 40))
-# uncomment if not using crops 
-#net.blobs['data'].reshape(1,3,120,120) 
-#net.blobs['data'].data[...] = transformer.preprocess('data', im)
-for i in range(10):
-  net.blobs['data'].data[i, ...] = transformer.preprocess('data', crops[i])
 
-out = net.forward() 
