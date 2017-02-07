bbaa3eae14966c774619775f9ab4467f19c34564
irenelfeng <irene.l.feng.17@dartmouth.edu>
2017-02-06 22:30:39 -0500

    column naming confusion

--- MATLAB/confusion_matrix.m
+++ MATLAB/confusion_matrix.m
@@ -10,8 +10,9 @@ function confusion_matrix(predY, testY, filename, titlee)
             Percent_Conf_mat(i,:) = Conf_mat(i,:)./sum(Conf_mat(i,:))*100; %sum across row: as in output class
         end
     end
-    labels = {'Angry','Fear', 'Happy', 'Sad', 'Surprise', 'Neutral'}; 
-    graph = heatmap(Percent_Conf_mat, labels, labels, 1,'Colormap', 'red','ShowAllTicks',1,'UseLogColorMap',true,'Colorbar',true);
+    % keep fer. i like it. 
+    labels = {'Anger','Disgust','Fear', 'Happy', 'Sad', 'Surprise', 'Neutral'}; 
+    graph = heatmap(Percent_Conf_mat, labels, labels, 1,'Colormap', 'red','ShowAllTicks',1,'UseLogColorMap',true);
     xlabel('Output Class'); ylabel('Target Class');
     title(titlee);
     saveas(graph, filename);
