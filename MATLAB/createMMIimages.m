50594ab440c1a218ce37abcd56ecdd87b56fc189
Irene <irene.l.feng.17@dartmouth.edu>
2017-02-13 01:07:54 -0500

    mmi loading scripts

---
+++ MATLAB/createMMIimages.m
@@ -0,0 +1,61 @@
+
+directory = '../../mmi';
+sessions = dir(directory);
+Y = [];
+disgustFrames = 100; 
+peakoffset = 0;
+
+for session = sessions'
+    files = dir([directory,'/',session.name,'/','*.avi']);
+    if length(files) == 1 % skips the ., .. nonsense
+        [~, fname] = fileparts(files(1).name);
+        neut = [];
+        frames = 25;
+        try
+            v = VideoReader([directory,'/', session.name,'/', files(1).name]);
+        catch
+            session.name, 'avi not working'
+            v =  VideoReader([directory,'/', session.name,'/', fname, '.mp4']);
+        end
+        for i=1:3
+            neut(:,:,:,i) = readFrame(v); 
+        end
+
+        DOMnode = xml2struct(xmlread([directory,'/',session.name,'/', fname, '.xml']));
+        emot_tag = DOMnode.ActionUnitCoding.Metatag(2);
+        emot = str2double(emot_tag{1}.Attributes.Value);
+        if emot <= 6 
+            % make a whatever you know? like a Y value. lmao 
+            if emot == 2
+                frames = disgustFrames;
+                peakoffset = disgustFrames/2;
+            end
+            
+            while v.hasFrame
+                video = readFrame(v);
+                if v.CurrentTime >= v.Duration/2 - peakoffset*.04; % peak is middle
+                    break
+                end
+            end
+            
+            for i = 1:frames  % 25 frames = 1 second % also not in the right order but it should be okay cause it's within the folder
+                if ~v.hasFrame
+                    break
+                end
+                mid = readFrame(v);
+                imwrite(mid, sprintf('%s/%s/%s_%d.png', directory, session.name, fname, i));
+                Y(end+1) = emot;
+            end
+        end
+        % this comes later because neutral comes later
+        for i=1:3
+            imwrite(uint8(neut(:,:,:,i)), sprintf('%s/%s/%s_neut%d.png', directory, session.name, fname, i));
+            Y(end+1) = 0;
+        end
+        fname, session.name
+    end
+end
+
+Y = mmi_to_fer(Y);
+save mmiY.mat Y
+    
--- PYTHON/FaceDetect/mmi_face_detect.py
+++ PYTHON/FaceDetect/mmi_face_detect.py
@@ -8,9 +8,14 @@ import numpy as np
 # Get user supplied values
 directory = '../../../mmi'
 cascPath = 'haarcascade_frontalface_default.xml'
+eyePath = 'haarcascade_eye_eyeglasses.xml'
 
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
+eyeCascade = cv2.CascadeClassifier(eyePath)
+rotateLeft3 = ['S028', 'S030', 'S031', 'S032', 'S033',
+'S035', 'S036', 'S037', 'S038', 'S039', 'S040', 'S041',
+'S042', 'S045', 'S046', 'S047', 'S048', 'S049', 'S050'];
 
 # save directory 
 facedir = directory+'/../face_detected'
@@ -23,6 +28,12 @@ for root, dirs, files in os.walk(directory):
             image = cv2.imread(root+'/'+file, 0) # autoconverts to grayscale
             # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             # Detect faces in the image
+            SUBJECT = file.split('-')[0]
+            if file.split('-')[0] == 'S021':
+                image = np.rot90(image) # rotate left once, stupid
+            elif SUBJECT in rotateLeft3: 
+                image = np.rot90(image,3) # rotate left 3 times, as in righht
+
             faces = faceCascade.detectMultiScale(
                 image,
                 scaleFactor=1.01,
@@ -30,28 +41,46 @@ for root, dirs, files in os.walk(directory):
                 minSize=(100, 100), # (30,30) for FER images 
             )
 
+
             if len(faces) == 0: 
-                # then we have to rotate the image to the left, i think
-                print 'found no face for file {0}, so i rotate!'.format(file) 
-                image = np.rot90(image,3)
-                faces = faceCascade.detectMultiScale(
-                    image,
-                    scaleFactor=1.01,
-                    minNeighbors=5,
-                    minSize=(100, 100), # (30,30) for FER images 
-                )
+                # ugh but it's not capturing all the rotated images because sometimes it detects something stupid as a face. 
+                # then we have to rotate the image 90 to the right, i think
+                # sad not all of them are rotated right. 
+                print 'found no face for file {0}'.format(file) 
 
             elif len(faces) > 1: 
                 print 'there is more than 1 face founded in file {0}'.format(file)
-                # maybe to make it better, do eye detect and choose the one with the biggest eyes. 
-                faces = sorted(faces, key=lambda x: (x[2],x[3]), reverse=True)
+                # maybe to make it better, do eye detect 
+                most_centered = 0
+                most_centered_dist = 1000
+                for i,(x,y,w,h) in enumerate(faces):
+                    crop_img = image[y:y+h, x:x+w]
+                    closest_distance = 1000
+                    most_centered = 0
+
+                    eyes = eyeCascade.detectMultiScale(crop_img)
+                    if len(eyes) == 2: # this is probably the face we want
+                        eyes = sorted(eyes, key=lambda x: (x[0])) # x coordinate is COLUMNS
+                        print '2 eyes for this face {0}'.format(i)
+                        # left eye nearest the center
+                        # or maybe the midpoint of left eye and right eye
+                        # left eye edge (left eye start + left eye end + right eye end)
+                        midpoint = (eyes[0][0] + eyes[0][2] + eyes[1][0])/2.0
+                        # choose closest midpoint
+                        if abs((x+w) / 2.0 - midpoint) < closest_distance: 
+                            most_centered = i
+                            closest_distance = abs((x+w) / 2.0 - midpoint)
+
+                print 'most centered face is index {0}'.format(most_centered)
+                faces[0] = faces[most_centered]
+
 
             # crops the image at this rectangle 
             (x, y, w, h) = faces[0]
-            # print (x, y, w, h)
-            crop_img = image[y:y+h, x:x+w] # Crop from x, y, w, h -> 100, 200, 300, 400
+            # why is the image still not saved as rotated? o.o
+            crop_img = image[y:y+h, x:x+w] # Crop from x, y, w, h 
             # NOTE: its img[y: y + h, x: x + w] and *not* img[x: x + w, y: y + h]
             # scales crop to be square, i guess the closest square <= area 
             s = int(round(math.sqrt(w*h)))
             square = cv2.resize(crop_img, (s,s))
-            cv2.imwrite(facedir+'/'+file, square)
+            cv2.imwrite(facedir+'/'+ root + '/'+file, square)
