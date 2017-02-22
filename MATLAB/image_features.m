ba0f6bd72bbc6c9c7088ab9a810dbed29eeeae92
irenelfeng <irene.l.feng.17@dartmouth.edu>
2017-02-22 11:24:17 -0500

    ready to do top half bottom half training for LDA

--- MATLAB/image_features.m
+++ MATLAB/image_features.m
@@ -1,8 +1,35 @@
-function features = image_features(imvec, numSizes, numOrientations)
+function features = image_features(imvec, numSizes, numOrientations, halve)
 J= reshape(imvec, [48 48])';
 [JetsMagnitude, JetsPhase, GridPosition] = GWTWgrid_Simple(J,0,0, 2*pi, numSizes, numOrientations); 
 % we can ignore grid pos because they are the same for every image 
-features = [JetsMagnitude(:); JetsPhase(:)];
-%% complex = 0 simple = 0 for 2nd param
-%% grid size 0 => 10*10 ugh i'll have to code it myself
+switch halve
+    case top
+        % for every column of Jets Magnitude (a different filter), reshape
+        % it and get only the top half of it
+        Total = zeros(numSizes * numOrientations * 200, 1);
+        for i = 0:size(JetsMagnitude, 2)-1
+            mags = reshape(JetsMagnitude(:, i+1), [10 10]);
+            phases = reshape(JetsPhase(:, i+1), [10 10]);
+            columnphases = phases(1:5, :);
+            columnmags = mags(1:5, :);
+            Total(i*100+1:i*100+51) = columnmags(:);
+            Total(i*100+51:i*100+101) = columnphases(:);
+        end
+        features = Mags;
+        
+    case bottom
+        Total = zeros(numSizes * numOrientations * 200, 1);
+        for i = 0:size(JetsMagnitude, 2)-1
+            mags = reshape(JetsMagnitude(:, i+1), [10 10]);
+            phases = reshape(JetsPhase(:, i+1), [10 10]);
+            columnphases = phases(5:10, :);
+            columnmags = mags(5:10, :);
+            Total(i*100+1:i*100+51) = columnmags(:);
+            Total(i*100+51:i*100+101) = columnphases(:);
+        end
+        features = Mags;
+    otherwise
+        features = [JetsMagnitude(:); JetsPhase(:)];
+    
+    %% complex = 0 simple = 0 for 2nd param
 end
--- MATLAB/trainClassifier.m
+++ MATLAB/trainClassifier.m
@@ -1,7 +1,7 @@
 % given X, Y, returns model and confusion matrix 
 % calls model_selection.m, confusion_matrix.m
 % change this line from LDA to SVM
-type = 'LDA';
+type = 'SVM';
 
 % gets date 
 d = date; 
@@ -26,19 +26,20 @@ testY = double(Y(setdiff(train+1:size(X,1), [disgust;neutral])));
 % testY = Y(setdiff(28710:32299, [disgust; neutral]));
 % comment line if you want to do your own training
 
+
+% comment next 1 and then uncomment next three if you want to specify
+[MDL, s, o, comps] = model_selection(trainX, trainY, 3, 8, 5, type);
+%s = 3;
+%o = 8; 
+%MDL = train_Mc_SVM(features,trainY);
+%comps = 0;
 features = zeros(size(trainX,1), s*o*200);
 for i=1:size(trainX,1)
     features(i, :) = image_features(trainX(i,:), s, o)'; % call image features 
 end  
 
-% comment next 1 and then uncomment next three if you want to do your own
-% training
-[MDL, s, o, comps] = model_selection(trainX, trainY, 1:3, [4, 8], 5, type);
-% s = 1;
-% o = 8; 
-% MDL = train_Mc_LDA(features,trainY);
 predY = MDL_predict(MDL, testX, s, o, comps, mean(features));
 
 save(sprintf('MDL_nonFER_PCA%ss%do%d', type, s, o), 'MDL');
-confusion_matrix(predY, testY, sprintf('confusionWholeTrainedNonFER%s-s%ds-o%d%s.png', type, s, o,d), ...
+confusion_matrix(predY, testY, sprintf('confusionWholeTrainedNonFER%s-s%d-o%d%s.png', type, s, o,d), ...
                 sprintf('Confusion WholeTrained NonFER %s-s%d-o%d', type, s, o));
