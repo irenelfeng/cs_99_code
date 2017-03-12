33ba107552298422e4c3e1ac6801d6fe36ff44db
irenelfeng <irene.l.feng.17@dartmouth.edu>
2017-03-12 11:21:50 -0400

    for LDA and SVM to cooperate, also w/ without Foveated

--- MATLAB/trainClassifier.m
+++ MATLAB/trainClassifier.m
@@ -1,15 +1,17 @@
 % given X, Y, returns model and confusion matrix 
 % calls model_selection.m, confusion_matrix.m
 % change this line from LDA to SVM
-type = 'LDA';
-halve = 'top'; % whole, top, or bottom. 
+type = 'SVM';
+halve = 'bottom'; % whole, top, or bottom.
+foveated = 1; % 0 or 1 if you want foveated
 % gets date 
 d = date; 
 
 % X and Y
-load('data/nonFER128X.mat');
-%load('topX.mat'); % images
-%load('bottomX.mat');
+% load('data/nonFER128X.mat');
+  %load('topX.mat'); % images
+load('data/top120X.mat');
+X = topX;
 load('data/nonFER128Y.mat');
 %load('Y.mat');  % labels
 % disgust = find(Y==1);
@@ -28,20 +30,26 @@ testY = double(Y(setdiff(train+1:size(X,1), [disgust;neutral])));
 
 
 % comment next 1 and then uncomment next two if you want to specify
-%[MDL, s, o, comps] = model_selection(trainX, trainY, 3, 8, 5, type, halve);
-% s = 3;
+% [MDL, s, o, comps] = model_selection(trainX, trainY, [3:4], 8, 5, type, halve);
+s = 5;
 % o = 8;
 features = [];
 for i=1:size(trainX,1)
-    features = [features; image_features(trainX(i,:), s, o, halve)']; % call image features 
+    features = [features; image_features(trainX(i,:), s, o, halve, foveated)']; % call image features 
 end 
-% also uncomment this if you want to specify comps. ran this. super similar 
-[comps, points, resid] = bestPCA(features); % or bestPCA
-MDL = train_Mc_LDA(points,trainY);
-% predY = MDL_predict(MDL, testX, s, o, halve, comps(:,1:79), mean(features));
-
-predY = MDL_predict(MDL, testX, s, o, halve, comps, mean(features));
-save(sprintf('compsp128f1_%s_nonFER_PCAss%do%d', halve, type, s, o), 'comps');
-save(sprintf('MDLp128f1_%s_nonFER_PCAss%do%d', halve, type, s, o), 'MDL');
+  
+if strcmp(type, 'LDA') == 0 % if not LDA
+    MDL = train_Mc_SVM(features, trainY);
+    predY = MDL_predict(MDL, testX, s, o, halve);
+else
+    % also uncomment this if you want to specify comps. ran this. super similar 
+    %[comps, points, resid] = bestPCA(features); \
+    % or just do
+    %pca(features);
+    % MDL = train_Mc_LDA(features,trainY);
+    predY = MDL_predict(MDL, testX, s, o, halve, comps, mean(features));
+    save(sprintf('compsp128f1_%s_nonFER_PCAss%do%d', halve, type, s, o), 'comps');
+end
+save(sprintf('MDLp128f1_%s_nonFER%ss%do%d', halve, type, s, o), 'MDL');
 confusion_matrix(predY, testY, sprintf('confusion%sTrainedNonFER%s-s%d-o%d%s128pf1.fig', halve, type, s, o,d), ...
                 sprintf('Confusion %sTrained NonFER%s-s%d-o%d', halve, type, s, o));
