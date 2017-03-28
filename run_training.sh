89b53fecbcf087c55f8bdfc6b70aa4551514d7c6
Irene L. Feng <ifeng@anthill.cs.dartmouth.edu>
2017-03-28 17:30:23 -0400

    training shell script for qsub

---
+++ run_training.sh
@@ -0,0 +1,2 @@
+cd /home/anthill/ifeng/cs99/caffe/
+./build/tools/caffe train -solver=models/mollahosseini_fer/from_googlenet_solver.prototxt -weights=models/mollahosseini_fer/training_snapshot_googlenet_quick_iter_100000.caffemodel > training_log
