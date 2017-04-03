783f57440adf059e8e2754a4f5e289bc48e51f45
Irene <irene.l.feng.17@dartmouth.edu>
2017-04-03 12:26:39 -0400

    seeing if bottom works with _lmdb testing: matlab does not work

--- MATLAB/create_matlab_folder.m
+++ MATLAB/create_matlab_folder.m
@@ -1,14 +1,25 @@
-load('/Users/irenefeng/Documents/Computer_Social_Vision/cs_99_code/MATLAB/data/128X.mat');
 % X.mat 
 % okay we have X.mat . then what? umm just output that into some files. 
-s = '../../CNN_48_images/MATLAB_val';
-mkdir(s)
-train = floor(9/10*size(X,1));
-trainX = X(1:train,:);
-testX = X(train+1:size(X,1), :);
-for i = 1:size(testX, 1)
-    im = reshape(testX(i,:), [128 128])';
-    im = imresize(im, [48, 48])/255.0; 
-    imwrite(im, sprintf('%s/%d.jpg', s, i))
+modes = {'bottom', 'top'}; % also '' for whole
+for i=1:length(modes)
+    if strcmp('bottom',modes{i}) == 1
+       load('data/bottom128X.mat'); 
+       X = bottomX;
+    elseif strcmp('top', modes{i}) == 1
+        load('data/top128X.mat'); 
+        X = topX;
+    else
+       load('data/128X.mat');  
+    end
+    s = ['../../CNN_48_images/MATLAB_val', modes{i}]
+    mkdir(s);
+    train = floor(9/10*size(X,1));
+    trainX = X(1:train,:);
+    testX = X(train+1:size(X,1), :);
+    for i = 1:size(testX, 1)
+        im = reshape(testX(i,:), [128 128])';
+        im = imresize(im, [48, 48])/255.0; 
+        imwrite(im, sprintf('%s/%d.jpg', s, i))
+    end
 end
 
--- PYTHON/caffe_scripts/test_network_with_lmdb.py
+++ PYTHON/caffe_scripts/test_network_with_lmdb.py
@@ -3,7 +3,9 @@ import caffe
 import numpy as np
 import scipy.io as sio
 import matplotlib.pyplot as plt
+import sys 
 
+which_val = sys.argv[1] # blurring_bottom, blurring_top, ft_
 LMDB_path = '/home/ifsdata/scratch/cooperlab/irene/CNN_48_images/LMDB/'
 mean_blob = caffe.io.caffe_pb2.BlobProto()
 with open(LMDB_path+'40_mean.binaryproto') as f:
@@ -12,52 +14,54 @@ mean_array = np.asarray(mean_blob.data, dtype=np.float32).reshape(
     (mean_blob.channels, mean_blob.height, mean_blob.width))
 mean = mean_array.mean(1).mean(1)
 
-lmdb_env = lmdb.open(LMDB_path+'val_shuf_lmdb_40')
+if 'bottom' in which_val:
+    lmdb_env = lmdb.open(LMDB_path+'bottom_val_lmdb_40')
+elif 'top' in which_val:
+    lmdb_env = lmdb.open(LMDB_path+'top_val_lmdb_40')
+else: 
+    lmdb_env = lmdb.open(LMDB_path+'val_shuf_lmdb_40')
 lmdb_txn = lmdb_env.begin()
 lmdb_cursor = lmdb_txn.cursor()
 
-accuracies = []
+PARENT_DIR = '/home/anthill/ifeng/cs99/'
+#PARENT_DIR = '/Users/irenefeng/Documents/Computer_Social_Vision/'
+CAFFE_DIR = PARENT_DIR + 'caffe/'
+net = caffe.Net(CAFFE_DIR + 'models/mollahosseini_fer/deploy.prototxt', 1,
+                                weights=CAFFE_DIR + 'models/mollahosseini_fer/snapshots/'+which_val+'_iter_1000000.caffemodel')
+transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
 
-for it in range(40000, 40000, 1000000):
-    PARENT_DIR = '/home/anthill/ifeng/cs99/'
-    #PARENT_DIR = '/Users/irenefeng/Documents/Computer_Social_Vision/'
-    CAFFE_DIR = PARENT_DIR + 'caffe/'
-    net = caffe.Net(CAFFE_DIR + 'models/mollahosseini_fer/deploy.prototxt', 1,
-                                    weights=CAFFE_DIR + 'models/mollahosseini_fer/snapshots/.caffemodel')
-    transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
-
-    transformer.set_mean('data', np.array(mean))
-    transformer.set_transpose('data', (2,0,1)) # i don't know what the transpose is for 
-    transformer.set_channel_swap('data', (2,1,0)) # from RGB to BGR order 
-    transformer.set_raw_scale('data', 255.0) # just in case not 0-255. 
+transformer.set_mean('data', np.array(mean))
+transformer.set_transpose('data', (2,0,1)) # i don't know what the transpose is for 
+transformer.set_channel_swap('data', (2,1,0)) # from RGB to BGR order 
+transformer.set_raw_scale('data', 255.0) # just in case not 0-255. 
 
 
-    predictions = []
-    values = []
-    for key, value in lmdb_cursor:
-        datum = caffe.proto.caffe_pb2.Datum()
-        datum.ParseFromString(value)
-        label = int(datum.label)
-        image = caffe.io.datum_to_array(datum)
-        image = image.astype(np.uint8)
-        # change channels from 3, 48, 48 to 48, 48, 3
-        image = np.swapaxes(image, 2, 0)
-        # image = np.swapaxes(image, 0, 1)
-        crops = caffe.io.oversample([image], (40, 40))
-            # uncomment if not using crops 
-            #net.blobs['data'].reshape(1,3,120,120) 
-            #net.blobs['data'].data[...] = transformer.preprocess('data', im)
-        for j in range(10):
-            net.blobs['data'].data[j, ...] = transformer.preprocess('data', crops[j])
-        
-        out = net.forward() 
-        #print out
-        p = np.argmax(np.average(out['prob'], axis=0))
-        predictions.append(p)
-        values.append(label)
+predictions = []
+values = []
+for key, value in lmdb_cursor:
+    datum = caffe.proto.caffe_pb2.Datum()
+    datum.ParseFromString(value)
+    label = int(datum.label)
+    image = caffe.io.datum_to_array(datum)
+    image = image.astype(np.uint8)
+    # change channels from 3, 48, 48 to 48, 48, 3
+    image = np.swapaxes(image, 2, 0)
+    # image = np.swapaxes(image, 0, 1)
+    crops = caffe.io.oversample([image], (40, 40))
+        # uncomment if not using crops 
+        #net.blobs['data'].reshape(1,3,120,120) 
+        #net.blobs['data'].data[...] = transformer.preprocess('data', im)
+    for j in range(10):
+        net.blobs['data'].data[j, ...] = transformer.preprocess('data', crops[j])
+    
+    out = net.forward() 
+    #print out
+    p = np.argmax(np.average(out['prob'], axis=0))
+    predictions.append(p)
+    values.append(label)
 
-    acc = (len(predictions) - np.count_nonzero(np.array(values) - np.array(predictions))) * 1.0 / len(predictions)
-    accuracies.append(acc)
+acc = (len(predictions) - np.count_nonzero(np.array(values) - np.array(predictions))) * 1.0 / len(predictions)
+print acc
 # sio.savemat('mollahosseini_test_results_whole_valmdb',
 #                 {'predY':np.array(predictions), 'testY':np.array(values)})
 
