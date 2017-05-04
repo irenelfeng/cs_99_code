18ba24e31995936b0dbe1af89d080fa9c559d383
Irene <irene.l.feng.17@dartmouth.edu>
2017-05-04 13:01:46 -0400

    naming of predY stuff

--- MATLAB/trainClassifier.m
+++ MATLAB/trainClassifier.m
@@ -1,35 +1,34 @@
 % given X, Y, returns model and confusion matrix 
 % calls model_selection.m, confusion_matrix.m
 % change this line from LDA to SVM
-type = 'SVM';
-mode = 'bottom'; % whole, top, bottom, local, global, blurred_top_, blurred_bottom_.
-foveated = 1;
+type = 'LDA';
+mode = 'whole'; % whole, top, bottom, local, global, blurred_top_, blurred_bottom_.
+foveated = 0;
 % gets date 
 d = date; 
 
 % X and Y
 if isempty(strfind(mode, 'blurred_'))
-    load('data/nonFER128X.mat');
+    load('data/128X.mat');
 else 
     if ~isempty(strfind(mode, 'bottom'))
-        load('data/bottom120X.mat');
+        load('data/bottom128X.mat');
         X = bottomX;
     else
-        load('data/top120X.mat');
+        load('data/top128X.mat');
         X = topX;
     end
 end
-load('data/nonFER128Y.mat');
+load('data/128Y.mat');
 train = floor(9/10*size(X,1));
 trainX = X(1:train,:);
 trainY = Y(1:train);
 testX = X(train+1:size(X,1), :);
 testY = double(Y(train+1:size(X,1)));
 
-
 % comment next 1 and then uncomment next two if you want to specify
-% [MDL, s, o, comps] = model_selection(trainX, trainY, [3:5], 8, 5, type, mode, foveated);
-s = 5;
+[MDL, s, o, comps] = model_selection(trainX, trainY, [2:5], 6, 5, type, mode, foveated);
+s = 3;
 o = 8;
 features = [];
 for i=1:size(trainX,1)
@@ -37,15 +36,19 @@ for i=1:size(trainX,1)
 end 
 % FOR LDA/PCA: also uncomment below lines if you want to specify comps. ran this. super similar 
 if strcmp(type, 'LDA') == 1 
-    % [comps, points, resid] = bestPCA(features); 
-    [comps, points, resid] = pca(features); 
+    [comps, points, resid] = bestPCA(features); 
+    % [comps, points, resid] = pca(features); 
     % or just regular pca if you know how many
     
-    MDL = train_Mc_LDA(points(:, 1:159),trainY);
+    MDL = train_Mc_LDA(points,trainY);
     predY = MDL_predict(MDL, testX, s, o, mode, foveated, comps(:,1:159), mean(features));
-    save(sprintf('MDLcomps_%s_nonFERs%ss%do%d', mode, type, s, o), 'comps');
+    f = '';
+    if foveated == 1
+        f = '_foveated';
+    end
+    save(sprintf('MDLcomps%s_%s_nonFERs%ss%do%d', f, mode, type, s, o), 'comps');
 else 
-    MDL = train_Mc_SVM(features,trainY);
+    MDL = traitrainClassifiern_Mc_SVM(features,trainY);
     predY = MDL_predict(MDL, testX, s, o, mode, foveated);
 end
 
