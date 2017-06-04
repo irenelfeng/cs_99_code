6409e4eb48861f94933dca542cf64f3d3c3c3bf3
Irene <irene.l.feng.17@dartmouth.edu>
2017-06-04 01:56:17 -0400

    adding and removing nonpertinent files

---
+++ MATLAB/tests/unit_test_model_selection.m
@@ -0,0 +1,20 @@
+% does a mini test run of test of model selection 
+
+load('data/128X.mat');
+load('data/128Y.mat');
+s = 1;
+o = 4;
+trainX = X(1:100,:);
+trainY = Y(1:100,:);
+
+% 0. checks for mode error 
+[MDL, s, o] = model_selection(trainX, trainY, s, o, 5, 'dumbo');
+assert(strcmp(MDL, 'error') == 1);
+
+% 1. does multiclass LDA 
+[MDL, s, o, comps] = model_selection(trainX, trainY, s, o, 5, 'LDA');
+
+% 2. does multiclass SVM 
+% [MDL, s, o] = model_selection(trainX, trainY, s, o, 5, 'SVM');
+
+disp('TESTS PASSED')
